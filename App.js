abb07b521924288adfc078e87662cc1dec0629e0
carloshilner <47983053+carloshilner@users.noreply.github.com>
2019-03-02 10:32:42 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { View, StyleSheet } from 'react-native'
+import Simples from './componentes/Simples'
+import { ParImpar } from "./componentes/ParImpar";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Simples texto='Flexivel!!'/>
+      </View>
+     
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },  
+})
---
+++ Simples.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+import Padrao from '../estilo/Padrao'
+
+//export default function(props){
+//    return <Text>{props.texto}</Text>
+//}
+
+//export default props => [
+//<Text key={1}> {props.texto} </Text>,
+//<Text key={2}> {props.texto} </Text>
+//]
+
+export default props => 
+    <Text style={[Padrao.ex]}> {props.texto}  </Text>

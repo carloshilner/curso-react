050a63739ece920a564ef479284c46677bf1d5fa
carloshilner <47983053+carloshilner@users.noreply.github.com>
2019-03-09 15:26:44 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
-import Simples from './componentes/Simples'
-import ParImpar from './componentes/ParImpar'
-import Inverter, {MegaSena} from './componentes/Multi'
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import params from './src/params'
+import Field from './src/components/Field'
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Simples texto='Flexivel!!'/>
-        <ParImpar numero={41} />
-        <Inverter texto='React Native!' />
-        <MegaSena numeros={50} />
-      </View>     
+        <Text style={styles.welcome}>Iniciando o Mines!?</Text>
+        <Text style={styles.instructions}>Tamanho da grade:
+        {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded />
+        <Field flagged />
+        <Field flagged opened  />
+      </View>
     )
   }
 }
@@ -22,5 +31,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },  
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 })
